ed6c43e46dd14f33d89ce0b5e3a9361d5b4ee62a
Kenny Wardlaw <kwardlaw102@gmail.com>
2023-06-29 12:31:32 -0400

    Started Unity project

---
+++ Unity/OperatorGame/Assets/Scripts/Operator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Operator
+{
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
+
+// Based on following link:
+// https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/how-to-create-a-new-method-for-an-enumeration
+public static class OperatorExtensions
+{
+    public static Operator Random(this Operator op)
+    {
+        Operator[] values = (Operator[]) System.Enum.GetValues(typeof(Operator));
+        return values[UnityEngine.Random.Range(0, values.Length)];
+    }
+}
